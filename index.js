e06fd6612f8de534b2d18f70c4d330ed5fb4723f
maxrabin <max.rabin17@gmail.com>
2021-04-27 19:09:12 -0400

    commit tree 982cce8b16cf75890fbaaf0647ea0f0d82f0745a

---
+++ index.js
@@ -0,0 +1,12 @@
+/*function divide(x=2000, y=100){
+    return x/y
+} */
+const x = 2000
+const y = 100
+const divide = function() {
+    return x/y
+}
+
+
+const square = x => x * x
+const add = (parameter1, parameter2) => parameter1 + parameter2
